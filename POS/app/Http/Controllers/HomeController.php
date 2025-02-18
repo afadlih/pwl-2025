2dd411f6dc2a1dae27238cff2cb797352d42cf9c
Ahmad Fadlih Wahyu Sardana <ahmadsardana55@sma.belajar.id>
2025-02-18 17:14:38 +0700

    Praktikum POS Controller

--- POS/app/Http/Controllers/HomeController.php
+++ POS/app/Http/Controllers/HomeController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('home');
+    }
+    public function choose()
+    {
+        return view('choose');
+    }
 }
