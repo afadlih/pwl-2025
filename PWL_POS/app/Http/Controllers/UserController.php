239cf292994e3d6bcf9078c8c9b323685059ccbe
Ahmad Fadlih Wahyu Sardana <ahmadsardana55@sma.belajar.id>
2025-03-04 10:31:31 +0700

    Praktikum 2.4 – Retreiving or Creating Models Jobsheet 4

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -10,11 +10,10 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrCreate([
-            'username' => 'manager22',
-            'nama' => 'Manager Dua Dua',
-            'password' => Hash::make('12345'),
-            'level_id' => 2
+        $user = UserModel::firstOrNew([
+            'username' => 'manager',
+            'nama' => 'Manager',
+
         ]);
         return view('user', ['data' => $user]);
     }
