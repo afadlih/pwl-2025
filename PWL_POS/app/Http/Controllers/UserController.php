722b6371179b89b1ae74fba4f036db57e706a3fa
Ahmad Fadlih Wahyu Sardana <ahmadsardana55@sma.belajar.id>
2025-03-04 09:40:36 +0700

    Praktikum 2.1 -  Retrieving Single Models Jobsheet 4

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -10,7 +10,16 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstWhere('level_id', 1);
-        return view ('user',['data'=>$user]);
+        $user = UserModel::findor(
+            1,
+            [
+                'username',
+                'nama',
+            ],
+            function () {
+                abort(404);
+            }
+        );
+        return view('user', ['data' => $user]);
     }
 }
