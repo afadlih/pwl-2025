ee2438af81ac6656c76f182237efb7c20f032748
Ahmad Fadlih Wahyu Sardana <ahmadsardana55@sma.belajar.id>
2025-03-04 11:12:36 +0700

    Praktikum 2.5 – Attribute Changes Jobsheet 4

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -11,29 +11,22 @@ class UserController extends Controller
     public function index()
     {
         $user = UserModel::create([
-            'username' => 'manager55',
-            'nama' => 'Manager Tiga Tiga',
+            'username' => 'manager11',
+            'nama' => 'Manager11',
             'password' => Hash::make('12345'),
             'level_id' => 2
 
         ]);
 
-        $user->username = 'manager56';
-
-        $user->isDirty();
-        $user->isDirty('username');
-        $user->isDirty('nama');
-        $user->isDirty('nama', 'username');
-
-        $user->isClean();
-        $user->isClean('username');
-        $user->isClean('nama');
-        $user->isClean('nama', 'username');
+        $user->username = 'manager12';
 
         $user->save();
-
-        $user->isDirty();
-        $user->isClean();
-        dd($user)->isDirty;
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged('username','level_id');
+        $user->wasChanged(['nama']);
+        $user->wasChanged(['nama','username']);
+        
+        
     }
 }
