36c9703ce3f2199da2a76a6bf8e87452eb1e72c1
Ahmad Fadlih Wahyu Sardana <ahmadsardana55@sma.belajar.id>
2025-03-11 09:59:17 +0700

    Praktikum 3 – Implementasi jQuery Datatable di AdminLTE  Jobsheet 5

--- PWL_POS/resources/views/user/index.blade.php
+++ PWL_POS/resources/views/user/index.blade.php
@@ -9,6 +9,14 @@
             </div>
         </div>
         <div class="card-body">
+            @if (session('success'))
+                <div class="alert alert-success">{{ session('success') }}</div>
+            @endif
+
+            @if (session('error'))
+                <div class="alert alert-danger">{{ session('error') }}</div>
+            @endif
+
             <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
                 <thead>
                     <tr>
@@ -21,54 +29,53 @@
                 </thead>
             </table>
         </div>
-    </div>
-@endsection
+    @endsection
 
-@push('css')
-@endpush
+    @push('css')
+    @endpush
 
-@push('js')
-    <script>
-        $(document).ready(function() {
-            var dataUser = $('#table_user').DataTable({
-                // serverSide: true, jika ingin menggunakan server side processing 
-                serverSide: true,
-                ajax: {
-                    "url": "{{ url('user/list') }}",
-                    "dataType": "json",
-                    "type": "POST"
-                },
-                columns: [{
-                    // nomor urut dari laravel datatable addIndexColumn() 
-                    data: "DT_RowIndex",
-                    className: "text-center",
-                    orderable: false,
-                    searchable: false
-                }, {
-                    data: "username",
-                    className: "",
-                    // orderable: true, jika ingin kolom ini bisa diurutkan  
-                    orderable: true,
-                    // searchable: true, jika ingin kolom ini bisa dicari 
-                    searchable: true
-                }, {
-                    data: "nama",
-                    className: "",
-                    orderable: true,
-                    searchable: true
-                }, {
-                    // mengambil data level hasil dari ORM berelasi 
-                    data: "level.level_nama",
-                    className: "",
-                    orderable: false,
-                    searchable: false
-                }, {
-                    data: "aksi",
-                    className: "",
-                    orderable: false,
-                    searchable: false
-                }]
+    @push('js')
+        <script>
+            $(document).ready(function() {
+                var dataUser = $('#table_user').DataTable({
+                    // serverSide: true, jika ingin menggunakan server side processing 
+                    serverSide: true,
+                    ajax: {
+                        "url": "{{ url('user/list') }}",
+                        "dataType": "json",
+                        "type": "POST"
+                    },
+                    columns: [{
+                        // nomor urut dari laravel datatable addIndexColumn() 
+                        data: "DT_RowIndex",
+                        className: "text-center",
+                        orderable: false,
+                        searchable: false
+                    }, {
+                        data: "username",
+                        className: "",
+                        // orderable: true, jika ingin kolom ini bisa diurutkan  
+                        orderable: true,
+                        // searchable: true, jika ingin kolom ini bisa dicari 
+                        searchable: true
+                    }, {
+                        data: "nama",
+                        className: "",
+                        orderable: true,
+                        searchable: true
+                    }, {
+                        // mengambil data level hasil dari ORM berelasi 
+                        data: "level.level_nama",
+                        className: "",
+                        orderable: false,
+                        searchable: false
+                    }, {
+                        data: "aksi",
+                        className: "",
+                        orderable: false,
+                        searchable: false
+                    }]
+                });
             });
-        });
-    </script>
-@endpush
+        </script>
+    @endpush
