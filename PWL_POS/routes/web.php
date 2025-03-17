2e90dfb8ddc77a3e5447875a5606ac0b2ecd8610
Ahmad Fadlih Wahyu Sardana <ahmadsardana55@sma.belajar.id>
2025-03-17 22:07:41 +0700

    Praktikum 1 - Modal Ajax Tambah Data (Data User) Jobsheet 6

--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -22,15 +22,22 @@ use App\Http\Controllers\BarangController;
 Route::get('/', [WelcomeController::class, 'index']);
 
 Route::group(['prefix' => 'user'], function () {
-    Route::get('/', [UserController::class, 'index']);          // menampilkan halaman awal user
-    Route::post('/list', [UserController::class, 'list']);      // menampilkan data user dalam bentuk json untuk datatables
-    Route::get('/create', [UserController::class, 'create']);   // menampilkan halaman form tambah user
-    Route::post('/', [UserController::class, 'store']);         // menyimpan data user baru
-    Route::get('/{id}', [UserController::class, 'show']);       // menampilkan detail user
-    Route::get('/{id}/edit', [UserController::class, 'edit']);  // menampilkan halaman form edit user
-    Route::put('/{id}', [UserController::class, 'update']);     // menyimpan perubahan data user
-    Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
+    Route::get('/', [UserController::class, 'index']);                  // menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);              // menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']);           // menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']);                 // menyimpan data user baru
+    Route::get('/create_ajax', [UserController::class, 'create_ajax']); // menampilkan halaman form tambah user (ajax)
+    Route::post('/ajax', [UserController::class, 'store_ajax']);        // menyimpan data user baru (ajax)
+    Route::get('/{id}', [UserController::class, 'show']);               // menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);          // menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);             // menyimpan perubahan data user
+    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']); // menampilkan halaman form edit user (ajax)
+    Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']); // menyimpan perubahan data user (ajax)
+    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']); // menampilkan konfirmasi hapus user (ajax)
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); // menghapus data user (ajax)
+    Route::delete('/{id}', [UserController::class, 'destroy']);         // menghapus data user
 });
+
 Route::group(['prefix' => 'level'], function () {
     Route::get('/', [LevelController::class, 'index']);          // menampilkan halaman awal level
     Route::post('/list', [LevelController::class, 'list']);      // menampilkan data level dalam bentuk json untuk datatables
