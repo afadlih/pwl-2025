0e95351ae9d11b4bb7a3e3cb9e108009e273f29f
Ahmad Fadlih Wahyu Sardana <ahmadsardana55@sma.belajar.id>
2025-03-17 22:17:45 +0700

    Praktikum 1 - Modal Ajax Tambah Data (Data User) Jobsheet 6

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Models\LevelModel;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
