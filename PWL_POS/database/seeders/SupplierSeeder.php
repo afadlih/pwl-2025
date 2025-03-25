34b4765142b5a1a53b61c257699c8f60f3cc4fce
Ahmad Fadlih Wahyu Sardana <ahmadsardana55@sma.belajar.id>
2025-03-25 14:46:45 +0700

    Refactor SupplierSeeder and update user index view to enhance data insertion and remove unused filter

--- PWL_POS/database/seeders/SupplierSeeder.php
+++ PWL_POS/database/seeders/SupplierSeeder.php
@@ -7,30 +7,39 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
 class SupplierSeeder extends Seeder
-{{
+{
     /**
-     * Run the database seeds.    * Run the database seeds.
+     * Run the database seeds.
      */
-    public function run(): voidd
+    public function run(): void
     {
-        $data = [
-            ['level_id' => 1, 'level_kode' => 'ADM', 'level_nama' => 'Administrator'],       [
-                'supplier_id' => 1,l_kode' => 'MNG', 'level_nama' => 'Manager'],supplier_id' => 1,
+        $levelData = [
+            ['level_id' => 1, 'level_kode' => 'ADM', 'level_nama' => 'Administrator'],
+            ['level_id' => 2, 'level_kode' => 'MNG', 'level_nama' => 'Manager'],
             ['level_id' => 3, 'level_kode' => 'STF', 'level_nama' => 'Staff/Kasir'],
         ];
-        DB::table('m_level')->insert($data);
-    }  ],
-}
+        DB::table('m_level')->insert($levelData);
+
+        $supplierData = [
+            [
+                'supplier_id' => 1,
+                'supplier_kode' => 'SUP1',
+                'supplier_nama' => 'Supplier 1',
                 'supplier_alamat' => 'Bandung',
             ],
             [
+                'supplier_id' => 2,
+                'supplier_kode' => 'SUP2',
+                'supplier_nama' => 'Supplier 2',
+                'supplier_alamat' => 'Jakarta',
+            ],
+            [
                 'supplier_id' => 3,
-                'supplier_kode' => 'AM',
-                'supplier_nama' => 'Ah Meng',
-                'supplier_id' => 3,> 'Surabaya',
                 'supplier_kode' => 'ELK3',
                 'supplier_nama' => 'Supplier Elektronik 3',
                 'supplier_alamat' => 'Surabaya',
             ],
         ];
-        DB::table('m_supplier')->insert($data);    }}
+        DB::table('m_supplier')->insert($supplierData);
+    }
+}
--- PWL_POS/resources/views/user/index.blade.php
+++ PWL_POS/resources/views/user/index.blade.php
@@ -17,24 +17,6 @@
             @if (session('error'))
                 <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
-
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="form-group row">
-                        <label class="col-1 control-label col-form-label">Filter:</label>
-                        <div class="col-3">
-                            <select class="form-control" id="level_id" name="level_id" required>
-                                <option value="">- Semua -</option>
-                                @foreach ($level as $item)
-                                    <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
-                                @endforeach
-                            </select>
-                            <small class="form-text text-muted">Level Pengguna</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
             <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
                 <thead>
                     <tr>
@@ -48,10 +30,9 @@
             </table>
         </div>
     </div>
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static"
+        data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 @endsection
-
-<div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static"
-    data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 
 @push('css')
 @endpush
@@ -71,28 +52,39 @@
                 ajax: {
                     "url": "{{ route('user.list') }}",
                     "dataType": "json",
-                    "type": "POST",
-                    "async": true,
-                    data: function(d) {
-                        d._token = "{{ csrf_token() }}";
-                        d.level_id = $('#level_id').val();
-                    }
+                    "type": "POST"
                 },
-                columns: [
-                    { data: 'DT_RowIndex', name: 'DT_RowIndex', orderable: false, searchable: false },
-                    { data: 'username', name: 'username' },
-                    { data: 'nama', name: 'nama' },
-                    { data: 'level.level_nama', name: 'level.level_nama' },
-                    { data: 'aksi', name: 'aksi', orderable: false, searchable: false }
+                columns: [{
+                        data: "DT_RowIndex",
+                        className: "text-center",
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        data: "username",
+                        className: "",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "nama",
+                        className: "",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "level.level_nama",
+                        className: "",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "aksi",
+                        className: "",
+                        orderable: false,
+                        searchable: false
+                    }
                 ]
-            });
-
-            $('#level_id').on('change', function() {
-                dataUser.ajax.reload();
-            });
-
-            $('#myModal').on('hidden.bs.modal', function () {
-                dataUser.ajax.reload();
             });
         });
     </script>
